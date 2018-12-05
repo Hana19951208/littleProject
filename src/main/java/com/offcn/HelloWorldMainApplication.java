2fce88c897ffb49c572b40016d9a34da0785fb54
Hana <1948978861@qq.com>
2018-12-05 21:01:57 +0800

    create project

---
+++ src/main/java/com/offcn/HelloWorldMainApplication.java
@@ -0,0 +1,21 @@
+package com.offcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 吴方恩
+ * @create 2018/12/1-14:02
+ */
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorldMainApplication.class);
+    }
+
+
+}
---
+++ src/main/java/com/offcn/controller/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.offcn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 吴方恩
+ * @create 2018/12/1-14:05
+ */
+
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+
+        return "hello world";
+    }
+}
